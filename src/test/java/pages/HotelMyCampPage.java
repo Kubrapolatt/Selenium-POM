0ce646a78a79d18221a9a970dc9e3435f23887cd
Kübra <93886341+Kubrapolatt@users.noreply.github.com>
2022-02-06 23:35:16 +0300

    Add files via upload

--- src/test/java/pages/HotelMyCampPage.java
+++ src/test/java/pages/HotelMyCampPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class HotelMyCampPage{
@@ -28,6 +29,36 @@ public class HotelMyCampPage{
 
     @FindBy(xpath = "//span[text()='ListOfUsers']")
     public WebElement başarılıGirişYazısı;
+
+    @FindBy(xpath = "//span[text()='Hotel Management']")
+    public WebElement hotelManagementLinki;
+
+    @FindBy(xpath = "//a[@href='/admin/HotelAdmin']")
+    public WebElement hotelListLinki;
+
+    @FindBy(xpath = "//a[@class='btn btn-circle btn-default']")
+    public WebElement addHotelLinki;
+
+    @FindBy(xpath = "//input[@id='Code']")
+    public WebElement addHotelCodeKutusu;
+
+   @FindBy(xpath = "//select[@class='form-control input-lg required']")
+   public WebElement addHotelDropdown;
+
+   @FindBy(xpath = "//button[@id='btnSubmit']")
+   public WebElement addHotelSaveButonu;
+
+
+   @FindBy(xpath = "//button[@class='btn btn-primary']")
+   public WebElement addHotelKayıtBaşarılıYazısıOkButonu;
+
+    public void girişYap(){
+     Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+     ilkLoginLinki.click();
+     usernameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
+     passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+      loginButonu.click();
+    }
 
     public void bekle(int saniye){
       try {
